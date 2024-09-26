1983a4587230dc64a31136d98e55f25f594404d5
Антон Аладьин <aladin.anton95@outlook.com>
2024-09-26 13:26:55 +0700

    Add server and refactoring

---
+++ baobei/frontend/src/main.ts
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+// Dynamic titles
+router.afterEach((to) => {
+    if (to.meta.title) {
+        document.title = to.meta.title as string
+    } else {
+        document.title = 'Baobei' // Default
+    }
+})
+
+
+app.mount('#app')
---
+++ baobei/frontend/src/router/index.ts
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        title: "Главная"
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: "О Нас"
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue'),
+      meta: {
+        title: "Авторизация"
+      }
+    },
+    {
+      path: '/:catchAll(.*)', // This will catch all unmatched routes
+      name: 'notFound',
+      component: () => import('../views/NotFoundView.vue'),
+      meta: {
+        title: "Страница не найдена"
+      }
+    },
+  
+  ]
+})
+
+export default router

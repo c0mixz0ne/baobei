d01377573688ce01c1835c76ec69f48f42390d62
Anton Aladin <xpxp@mail.ru>
2025-01-10 01:07:45 +0700

    updates

---
+++ baobei/frontend/src/components/ProgramComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="programs">
+        <div class="program">
+            <slot></slot>
+        </div>
+    </section>
+</template>

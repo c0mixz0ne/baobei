4b455386c2d5b133355a24e9f37b9aac844083db
Anton Aladin <xpxp@mail.ru>
2025-01-18 00:26:44 +0700

    Updates

---
+++ baobei/frontend/src/views/GroupView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import WelcomeComponent from '@/components/WelcomeComponent.vue';
+</script>
+<template>
+    <WelcomeComponent class="groups">
+        <h1>Набор в группы 2025-2026 учебный год</h1>
+    </WelcomeComponent>
+</template>
+<style land="scss" scoped>
+</style>
